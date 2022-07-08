1e9f444ce31b2beed3c8c6937a6cb3ae986d4de7
hojat.ghasemi <hojat72elect@gmail.com>
2022-07-08 19:22:54 -0400

    initial implementation of the camera

--- app/src/main/java/ca/on/sudbury/hojat/smartcamera/MainActivity.kt
+++ app/src/main/java/ca/on/sudbury/hojat/smartcamera/MainActivity.kt
@@ -1,11 +1,182 @@
 package ca.on.sudbury.hojat.smartcamera
 
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import ca.on.sudbury.hojat.smartcamera.databinding.ActivityMainBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var imageCapture: ImageCapture? = null
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        outputDirectory = getOutPutDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        // Asking for permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                Constants.REQUIRED_PERMISSIONS,
+                Constants.REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        binding.btnTakePhoto.setOnClickListener {
+            takePhoto()
+        }
+    }
+
+    /**
+     * @return the internal directory of the app which will be deleted
+     * if user deletes this app.
+     */
+    private fun getOutPutDirectory(): File {
+
+        val mediaDir = externalMediaDirs.firstOrNull()?.let { mFile ->
+            File(mFile, resources.getString(R.string.app_name)).apply {
+                mkdirs()
+            }
+        }
+
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    // takes and also saves the pic to device
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(
+                Constants.FILE_NAME_FORMAT,
+                Locale.getDefault()
+            )
+                .format(
+                    System
+                        .currentTimeMillis()
+                ) + ".jpg"
+        )
+
+        // The options for saving the pic
+        val outputOption = ImageCapture
+            .OutputFileOptions
+            .Builder(photoFile)
+            .build()
+
+        imageCapture.takePicture(
+            outputOption, ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+
+                    val savedUri = Uri.fromFile(photoFile)
+                    val msg = "Photo Saved"
+
+                    Toast.makeText(
+                        this@MainActivity,
+                        "$msg $savedUri",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(
+                        Constants.TAG,
+                        "onError: ${exception.message}", exception
+                    )
+                    Toast.makeText(this@MainActivity, "couldn't save the image\ntry again later.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        )
+    }
+
+    private fun startCamera() {
+
+        // we're getting access to the camera in a way
+        // that can be bound to any lifeCycle owner.
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+
+            // this should be defined as a singleton.
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also { mPreview ->
+                    mPreview.setSurfaceProvider(
+                        binding.viewFinder.surfaceProvider
+                    )
+                }
+
+            imageCapture = ImageCapture.Builder().build()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this,
+                    cameraSelector,
+                    preview,
+                    imageCapture
+                )
+            } catch (e: Exception) {
+                Log.e(Constants.TAG, "startCamera failed", e)
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Constants.REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user", Toast.LENGTH_SHORT)
+                    .show()
+                finish()
+            }
+        }
+
+    }
+
+    private fun allPermissionsGranted() = Constants.REQUIRED_PERMISSIONS.all { permission ->
+        ContextCompat.checkSelfPermission(
+            baseContext, permission
+        ) == PackageManager.PERMISSION_GRANTED
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 }
